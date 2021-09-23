acca0f9bca79c41e407d702197b14e1adb4dcb2d
ebuche-murloc <immortalmagicdend@gmail.com>
2021-09-23 11:18:41 +0500

    Initial commit

---
+++ src/main/java/com/example/demoweb/controller/PostsViewController.java
@@ -0,0 +1,33 @@
+package com.example.demoweb.controller;
+
+import com.example.demoweb.model.Post;
+import com.example.demoweb.service.PostService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+import java.lang.Long;
+import java.util.List;
+
+@Controller
+public class PostsViewController {
+    @Autowired
+    PostService postsService;
+
+    @ResponseBody
+    @RequestMapping(path = "/post/{id}", method = RequestMethod.GET)
+    public String single(@PathVariable("id") Long id) {
+        return "Здесь будет страница поста №" + id;
+    }
+
+    @RequestMapping(path = "/", method = RequestMethod.GET)
+    public String list(Model model) {
+        model.addAttribute("appName", "Моё супер приложение");
+        model.addAttribute("posts", postsService.listAllPosts());
+        return "list";
+    }
+
+}
